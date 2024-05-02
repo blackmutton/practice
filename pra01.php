d74467b471fe25ad4e5b50fa6be26b5e1846c5a9
yt Chen <yutongc30@gmail.com>
2024-05-02 20:37:56 +0800

    while的蒐字練習

--- pra01.php
+++ pra01.php
@@ -30,6 +30,29 @@ echo '迴圈次數:'.$count;
 echo '<hr>';
 echo '<br>';
 ?>
+<h2>尋找字元(使用while)</h2>
+
+<ul>
+    <li>給定一個字串句子</li>
+    <li>給定一個單字或字母</li>
+    <li>尋找相符的內容</li>
+    <li>印出尋找到的單字或字母所在句子中的位置</li>
+    <p> </p>
+</ul>
+<?php
+$str="我到後面依然無法說她的願望是錯誤的，考量到她的經歷，所有否定的話語都是那麼蒼白無力";
+$target="否定";
+/* 取出部分字串？
+計算字串長度 */
+$position=0;
+// mb_substr只能取指定的位置，而不能跟mb_strpos一樣直接找到
+while($target!=mb_substr($str,$position,mb_strlen($target))){
+    echo "p=".$position;
+    echo ", substr= ".mb_substr($str,$position,mb_strlen($target));
+    echo "<br>";
+    $position++;
+}
+?>
 
 </body>
 </html>
