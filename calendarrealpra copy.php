3417b6f3f341de67a849efea264275775df4095b
yt Chen <yutongc30@gmail.com>
2024-05-10 16:46:26 +0800

    再次嘗試釐清月曆的程式邏輯

--- calendarrealpra copy.php
+++ calendarrealpra copy.php
@@ -4,153 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        table{
-            border-collapse: collapse;
-            border:3px double blue;
-        }
-        td{
-            padding:5px 10px;
-            border:1px solid lightgreen;
-
-        }
-
-.block-table{
-    width:380px;
-    display:flex;
-    flex-wrap:wrap;
-}
-.item{
-    margin-left:-1px;
-    margin-top:-1px;
-    display:inline-block;
-    width:50px;
-    height:50px;
-    border:1px solid lightgreen;
-    position:relative;
-    transition: all 0.3s;
-    background:white;
-}
-.item-header{
-    margin-left:-1px;
-    margin-top:-1px;
-    display:inline-block;
-    width:50px;
-    height:25px;
-    border:1px solid lightgreen;
-    text-align: center;
-    background-color: darkgreen; 
-    color:lightgreen
-}
-.item:hover{
-    background:yellow;
-    transform: scale(1.3);
-    font-weight:bold;
-    color:blue;
-    transition: all 0.3s;
-    z-index:10;
-
-}
-
-.holiday{
-    background:pink;
-}
-     
-    </style>
 </head>
 <body>
- <h2>萬年曆</h2>
-<form action="?" method="get">
-    <label for="month"></label>
-    <input type="number" name='month' value="<?=date("m");?>">
-    <input type="submit" value="送出">
-</form>
-<?php 
-//$month=(isset($_GET['month']))?$_GET['month']:date("m");    
-$month=$_GET['month']??date("m");
-
-//$year=(isset($_GET['year']))?$_GET['year']:date("Y");
-$year=$_GET['year']??date("Y");
-
-
-echo "年".$year;
-echo "<BR>";
-echo "月份:".$month;
-echo "<br>";
-$firstDay=strtotime(date("$year-$month-1"));
-$firstWeekStartDay=date("w",$firstDay);
-echo "第一周的開始是第".$firstWeekStartDay."日";
-$days=date("t",$firstDay);
-$lastDay=strtotime(date("Y-$month-$days"));
-echo "<br>";
-echo "最後一天是".date("Y-m-d",$lastDay);
-
-$birthday='1974-4-1';
-
-$days=[];
-for($i=0;$i<42;$i++){
-    $diff=$i-$firstWeekStartDay;
-    $days[]=date("Y-m-d",strtotime("$diff days",$firstDay));
-}
-
-if($month-1<1){
-    $prev=12;
-    $prev_year=$year-1;
-}else{
-    $prev=$month-1;
-    $prev_year=$year;
-}
-
-if($month+1>12){
-    $next=1;
-    $next_year=$year+1;
-
-}else{
-    $next=$month+1;
-    $next_year=$year;
-}
-
-?>
-<div style="width:384px;">
-    <a href="calendar.php?year=<?=$prev_year;?>&month=<?=$prev;?>" style="float:left">上一個月</a>
-    <a href="calendar.php?year=<?=$next_year;?>&month=<?=$next;?>" style="float:right">下一個月</a>
-</div>
-
-<div style='clear:both'></div>
-<h3><?=$year;?>年 <?=$month;?>月</h3>
-<?php
-
-echo "<div class='block-table'>";
-echo "<div class='item-header'>日</div>";
-echo "<div class='item-header'>一</div>";
-echo "<div class='item-header'>二</div>";
-echo "<div class='item-header'>三</div>";
-echo "<div class='item-header'>四</div>";
-echo "<div class='item-header'>五</div>";
-echo "<div class='item-header'>六</div>";
-foreach($days as $day){
-    $format=explode("-",$day)[2];
-    $w=date("w",strtotime($day));
-    if($w==0 || $w==6){
-
-        echo "<div class='item holiday'>$format</div>";
-    }else{
-
-        echo "<div class='item'>";
-        echo "<div class='date'>$format</div>";
-        echo "</div>";
+    <div>
+        <h2>萬年曆</h2>
+        <div>
+            <form action="" method="get">
+                <label for="year">Year:</label>
+                <input type="number" name="year" id="year" value="<?=date('Y')?>">
+                <label for="month">Month:</label>
+                <input type="number" name="month" id="year" value="<?=date('m')?>">
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+    <?php
+    $month=(isset($_GET['month']))?$_GET['month']:date('m');
+    $year=(isset($_GET['year']))?$_GET['year']:date('Y');
+    $firstDay=strtotime(date("$year-$month-1"));
+    $days=date("t",$firstDay);
+    $firstStartDay=date("w",$firstDay);
+    $specialDays = [
+        "01" => ["01" => "元旦"],
+        "02" => ["07" => "但丁生日"],
+        "05" => ["01" => "國際勞動節",
+                "04" => "文藝節"],
+        "12" => ["25" => "聖誕節"]
+        ];
+    $dates=[];
+    for($i=0;$i<42;$i++){
+        if($i>=$firstStartDay&&$i-$firstStartDay+1<=$days){
+            $diff=$i-$firstStartDay;
+            $dates[]=date("Y-m-d",strtotime("$diff days", $firstDay));
+        }else{
+            $dates[]="&nbsp;"
+        }
     }
-}
-echo "</div>";
-
-?>
-
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+    if(($month-1)<1){
+        $prev=12;
+        $prev_year=$year-1;
+    }else{
+        $prev=$month-1;
+        $prev_year=$year;
+    }
+    if(($month+1)>12){
+        $next=1;
+        $next_year=$year+1;
+    }else{
+        $next=$month+1;
+        $next_year=$year;
+    }
+    ?>
+    <div>
+        <a href="calendarrealpra1.php?year=<?=$prev_year?>&&month=<?=$prev?>">previous</a>
+        <?=$year?>年<?=$month?>月
+        <a href="calendarrealpra1.php?year=<?=$next_year?>&&month=<?=$next?>">next</a>
+    </div>
+    <div class="content">
+        <div class="flex">
+            <div class="item holiday">日</div>
+            <div class="item workday">一</div>
+            <div class="item workday">二</div>
+            <div class="item workday">三</div>
+            <div class="item workday">四</div>
+            <div class="item workday">五</div>
+            <div class="item workday">六</div>
+            <?php
+            
+            ?>
+        </div>
+    </div>
 </body>
 </html>
